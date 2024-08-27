c72c95b316ce591749b16b28c925cc81cfebf091
webprodigies <webprodigies.connect@gmail.com>
2024-08-27 13:02:53 -0400

    fix: removeing required props with ts ignore

--- src/app/(discover)/layout.tsx
+++ src/app/(discover)/layout.tsx
@@ -7,6 +7,7 @@ type ExploreLayoutProps = {
 const DiscoverLayout = ({ children }: ExploreLayoutProps) => {
   return (
     <div className="flex flex-col min-h-screen bg-black pb-10">
+      {/* @ts-ignore */}
       <Navbar />
       {children}
     </div>
